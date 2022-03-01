d8a5ba9127bfa968970d0daf6052cbce5f081c41
pallaviagnito <86787745+pallaviagnito@users.noreply.github.com>
2022-03-01 15:14:32 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WizardComponent } from './wizard/wizard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WizardComponent
   ],
   imports: [
     BrowserModule,
